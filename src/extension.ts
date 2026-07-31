7f7adde028dc60b8c6c4244e860d09def25e2a86
IvanSmahin72 <ivan_smahin@valentina-db.com>
2026-07-31 21:26:28 +0300

    fix: separate contributed pin title actions

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,16 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'vscode-pin-editor-action.pinEditor',
+			() => vscode.commands.executeCommand('workbench.action.pinEditor'),
+		),
+		vscode.commands.registerCommand(
+			'vscode-pin-editor-action.unpinEditor',
+			() => vscode.commands.executeCommand('workbench.action.unpinEditor'),
+		),
+	);
+}
 
 export function deactivate() {}
